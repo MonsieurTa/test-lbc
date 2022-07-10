7e818d8d28e0697a6ad82e5cbd5c16add0500953
William Ta <willt29000@gmail.com>
2022-07-10 14:04:14 +0200

    chore: add tests

--- pkg/fizzbuzz/fizzbuzz_test.go
+++ pkg/fizzbuzz/fizzbuzz_test.go
@@ -7,17 +7,35 @@ import (
 )
 
 func TestConfig(t *testing.T) {
-	service, err := New(Config{})
+	tests := []struct {
+		cfg      Config
+		expected string
+	}{
+		{
+			cfg:      Config{limit: 0},
+			expected: "limit must be > 0",
+		},
+		{
+			cfg:      Config{limit: -1},
+			expected: "limit must be > 0",
+		},
+		{
+			cfg:      Config{limit: 15, int1: 0, int2: 0, fizz: "fizz", buzz: "buzz"},
+			expected: "int1 can't 0",
+		},
+		{
+			cfg:      Config{limit: 15, int1: 1, int2: 0, fizz: "fizz", buzz: "buzz"},
+			expected: "int2 can't 0",
+		},
+	}
 
-	assert.Nil(t, service)
-	assert.Error(t, err)
-	assert.Equal(t, "limit must be > 0", err.Error())
+	for _, test := range tests {
+		service, err := New(test.cfg)
 
-	service, err = New(Config{limit: 1, int1: -1})
-
-	assert.Nil(t, service)
-	assert.Error(t, err)
-	assert.Equal(t, "input integers must be >= 0", err.Error())
+		assert.Nil(t, service)
+		assert.Error(t, err)
+		assert.Equal(t, test.expected, err.Error())
+	}
 }
 
 func TestFizzBuzz(t *testing.T) {
@@ -28,14 +46,35 @@ func TestFizzBuzz(t *testing.T) {
 }
 
 func TestFizzBuzzGenerate(t *testing.T) {
-	service, err := New(Config{limit: 15, int1: 3, int2: 5, fizz: "fizz", buzz: "buzz"})
-
-	assert.NotNil(t, service)
-	assert.Nil(t, err)
-
-	actual := service.Generate()
-	expected := []string{
-		"1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz",
+	tests := []struct {
+		cfg      Config
+		expected []string
+	}{
+		{
+			cfg:      Config{limit: 15, int1: 3, int2: 5, fizz: "fizz", buzz: "buzz"},
+			expected: []string{"1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz"},
+		},
+		{
+			cfg:      Config{limit: 10, int1: 2, int2: 2, fizz: "fizz", buzz: "buzz"},
+			expected: []string{"1", "fizzbuzz", "3", "fizzbuzz", "5", "fizzbuzz", "7", "fizzbuzz", "9", "fizzbuzz"},
+		},
+		{
+			cfg:      Config{limit: 10, int1: 1, int2: 2, fizz: "fizz", buzz: "buzz"},
+			expected: []string{"fizz", "fizzbuzz", "fizz", "fizzbuzz", "fizz", "fizzbuzz", "fizz", "fizzbuzz", "fizz", "fizzbuzz"},
+		},
+		{
+			cfg:      Config{limit: 10, int1: 1, int2: 2, fizz: "", buzz: ""},
+			expected: []string{"", "", "", "", "", "", "", "", "", ""},
+		},
 	}
-	assert.Equal(t, actual, expected)
+
+	for _, test := range tests {
+		service, err := New(test.cfg)
+
+		assert.NotNil(t, service)
+		assert.Nil(t, err)
+
+		actual := service.Generate()
+		assert.Equal(t, actual, test.expected)
+	}
 }
