c0ffade32247ade9f99a45c5766475692ab003dd
William Ta <willt29000@gmail.com>
2022-07-10 14:34:36 +0200

    refactor: enhance performance

--- pkg/fizzbuzz/fizzbuzz.go
+++ pkg/fizzbuzz/fizzbuzz.go
@@ -27,7 +27,6 @@ func (c *Config) valid() error {
 	if c.int2 == 0 {
 		return errors.New("int2 can't 0")
 	}
-
 	return nil
 }
 
@@ -41,25 +40,40 @@ func New(cfg Config) (*Service, error) {
 		return nil, err
 	}
 
+	cfg.int1 = abs(cfg.int1)
+	cfg.int2 = abs(cfg.int2)
+
 	return &Service{cfg: &cfg}, nil
 }
 
 func (s *Service) Generate() []string {
-	var v string
 	rv := make([]string, s.cfg.limit)
 
-	fizzbuzz := s.cfg.fizz + s.cfg.buzz
+	int1Multiple, int2Multiple := 0, 0
 	for i := 1; i <= s.cfg.limit; i++ {
-		if i%s.cfg.int1 == 0 && i%s.cfg.int2 == 0 {
-			v = fizzbuzz
-		} else if i%s.cfg.int1 == 0 {
-			v = s.cfg.fizz
-		} else if i%s.cfg.int2 == 0 {
-			v = s.cfg.buzz
-		} else {
-			v = strconv.Itoa(i)
+		int1Multiple += 1
+		int2Multiple += 1
+
+		if int1Multiple != s.cfg.int1 && int2Multiple != s.cfg.int2 {
+			rv[i-1] += strconv.Itoa(i)
+			continue
 		}
-		rv[i-1] = v
+
+		if int1Multiple == s.cfg.int1 {
+			rv[i-1] += s.cfg.fizz
+			int1Multiple = 0
+		}
+		if int2Multiple == s.cfg.int2 {
+			rv[i-1] += s.cfg.buzz
+			int2Multiple = 0
+		}
 	}
 	return rv
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
