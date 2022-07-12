c40022b91d547e5c191e927d693b788c37baa2a4
William Ta <willt29000@gmail.com>
2022-07-12 13:48:53 +0200

    refactor: update types and implement string based method

--- pkg/fizzbuzz/fizzbuzz.go
+++ pkg/fizzbuzz/fizzbuzz.go
@@ -3,12 +3,13 @@ package fizzbuzz
 import (
 	"errors"
 	"strconv"
+	"strings"
 )
 
 type Config struct {
-	Limit int32  `schema:"limit" json:"limit"`
-	Int1  int32  `schema:"int1" json:"int1"`
-	Int2  int32  `schema:"int2" json:"int2"`
+	Limit int    `schema:"limit" json:"limit"`
+	Int1  int    `schema:"int1" json:"int1"`
+	Int2  int    `schema:"int2" json:"int2"`
 	Str1  string `schema:"str1" json:"str1"`
 	Str2  string `schema:"str2" json:"str2"`
 }
@@ -29,9 +30,9 @@ func (cfg *Config) valid() error {
 }
 
 type Service struct {
-	limit int32
-	int1  int32
-	int2  int32
+	limit int
+	int1  int
+	int2  int
 	str1  string
 	str2  string
 }
@@ -51,11 +52,44 @@ func New(cfg Config) (*Service, error) {
 	}, nil
 }
 
-func (s *Service) Generate() []string {
+func (s *Service) String() string {
+	var builder strings.Builder
+
+	builder.WriteRune('[')
+	int1Multiple, int2Multiple := 0, 0
+	for i := 1; i <= s.limit; i++ {
+		int1Multiple++
+		int2Multiple++
+
+		if i == 1 {
+			builder.WriteString(`"`)
+		} else {
+			builder.WriteString(`,"`)
+		}
+
+		if int1Multiple != s.int1 && int2Multiple != s.int2 {
+			builder.WriteString(strconv.FormatInt(int64(i), 10))
+		} else {
+			if int1Multiple == s.int1 {
+				builder.WriteString(s.str1)
+				int1Multiple = 0
+			}
+			if int2Multiple == s.int2 {
+				builder.WriteString(s.str2)
+				int2Multiple = 0
+			}
+		}
+		builder.WriteString(`"`)
+	}
+	builder.WriteRune(']')
+	return builder.String()
+}
+
+func (s *Service) Array() []string {
 	rv := make([]string, s.limit)
 
-	int1Multiple, int2Multiple := int32(0), int32(0)
-	for i := 1; i <= int(s.limit); i++ {
+	int1Multiple, int2Multiple := 0, 0
+	for i := 1; i <= s.limit; i++ {
 		int1Multiple++
 		int2Multiple++
 
@@ -76,7 +110,7 @@ func (s *Service) Generate() []string {
 	return rv
 }
 
-func abs(x int32) int32 {
+func abs(x int) int {
 	if x < 0 {
 		return -x
 	}
